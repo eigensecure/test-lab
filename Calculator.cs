1474bed5e679594440d9cac92cd899f91f2a7e05
abhishek-eigensecure <abhishek.samuel@eigensecure.com>
2026-01-08 19:52:10 +0530

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -4,27 +4,6 @@ namespace GitPracticeLab
 {
     public class Calculator
     {
-        public double Add(double a, double b)
-        {
-            return a + b;
-        }
-
-        public double Multi(double a, double b)
-        {
-            return a * b;
-        }
-
-        public double Mod(double a, double b)
-        {
-            return a % b;
-
-        }
-
-        public double Sub(double a, double b)
-        {
-            return a % b;
-
-        }
 
         // Try adding new methods here (e.g., Subtract, Divide)
     }
